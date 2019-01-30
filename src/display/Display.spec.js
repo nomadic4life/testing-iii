87927fd401df6a208c38522d483fa92875b0d3d0
Dennis Orbison <orbison.dennis@gmail.com>
2019-01-30 13:25:58 -0800

    made failing test and it works

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,22 +1,27 @@
 // Test away!
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
+import Display from './Display';
+import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
 
-import Display from './Display';
+/* Tests:
+  - [X] displays if gate is open/closed and if it is locked/unlocked
+    - [X] Gate Closed and Locked
+    - Gate Closed and Unlocked
+    - Gate Open and Unlocked
+  - displays 'Closed' if the closed prop is true and 'Open' if otherwise
+  - displays 'Locked' if the locked prop is true and 'Unlocked' if othewise
+  - when locked or closed use the red-led class
+  - when unlocked or open use the green-led class
+*/
 
 describe('<Display />', () => {
-  it('renders the count of balls and strikes passed in', () => {
-    const { getByText, debug } = render(<Display locked={false} closed={false} />);
-    debug();
-    // console.log('body', document.body.outerHTML);
+  it('Gate Closed and Locked', () => {
+    const { getByText } = render(<Display closed={true} locked={true} />);
 
-    getByText(/3 balls/i);
-    getByText(/2 strikes/i);
-  });
-
-  it('unmounts component after each test', () => {
-    console.log(document.body.outerHTML);
+    getByText(/closed/i);
+    getByText(/locked/i);
   });
 });
 
